2c7826ba8a0c4191c2de556b1e011ed8ec6b1ac4
Navjot <virksaabnavjot@gmail.com>
2017-07-25 17:20:48 +0000

    scaffold pat validate

--- app/models/pat.rb
+++ app/models/pat.rb
@@ -1,2 +1,8 @@
 class Pat < ActiveRecord::Base
+    validates :appointment, :uniqueness => { :message => "appointment already taken" }
+validates :name, :presence => true
+validates :dob, :presence => true
+validates :condition, :presence => true
+
+
 end
