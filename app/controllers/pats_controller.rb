081fbb27b279ab87a3e90ec121332c8e75ec61ce
Navjot <virksaabnavjot@gmail.com>
2017-07-25 17:54:16 +0000

    devise done

--- app/controllers/pats_controller.rb
+++ app/controllers/pats_controller.rb
@@ -1,5 +1,6 @@
 class PatsController < ApplicationController
   before_action :set_pat, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
 
   # GET /pats
   # GET /pats.json
